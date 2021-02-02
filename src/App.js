44bf1dc3e1badb9dd43050ce7446515ca5bb3d3d
Erin Doherty <erin.doherty@team.telstra.com>
2021-02-02 13:18:52 +1100

    prices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { TimeSeries } from "pondjs";
 import {
   Charts,
@@ -11,71 +11,133 @@ import {
   styler,
   Legend,
 } from "react-timeseries-charts";
+import Feedback from "feeder-react-feedback"; // import Feedback component
+import "feeder-react-feedback/dist/feeder-react-feedback.css"; // import stylesheet
+import useInterval from "use-interval";
+import TimeAgo from "react-timeago";
 
 function App() {
   const [series, setSeries] = useState(null);
   const [columns, setColumns] = useState(null);
+  const [seriesPrices, setSeriesPrices] = useState(null);
+  const [columnsPrices, setColumnsPrices] = useState(null);
   const [style, setStyle] = useState(null);
   const [legend, setLegend] = useState(null);
   const [most, setMost] = useState(null);
+  const [mostPrices, setMostPrices] = useState(null);
   const [selection, setSelection] = useState();
   const [highlight, setHighlight] = useState();
-  useEffect(() => {
-    fetch(
-      "https://43m0bmc7g2.execute-api.ap-southeast-2.amazonaws.com/dev/counts"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let most = 0;
-        let columns = [];
-        columns.push("time");
-        for (const d of data) {
-          for (const dd in d) {
-            if (dd === "ts_end") continue;
-            if (dd === "timestamp") continue;
-            if (parseInt(d[dd]) <= 20) continue;
-            if (parseInt(d[dd]) > most) most = parseInt(d[dd]);
-            if (!columns.includes(dd)) columns.push(dd);
+  const [timerange, setTimerange] = useState();
+  const [updated, setUpdated] = useState();
+  useInterval(
+    () => {
+      fetch("https://wallstreetbets.s3-ap-southeast-2.amazonaws.com/wsb.json")
+        .then((response) => response.json())
+        .then((rawData) => {
+          setUpdated(rawData.updatedAt);
+          const data = rawData.data;
+          let most = 0;
+          let columns = [];
+          columns.push("time");
+          for (const d of data) {
+            for (const dd in d) {
+              if (dd === "ts_end") continue;
+              if (dd === "timestamp") continue;
+              if (parseInt(d[dd]) <= 20) continue;
+              if (parseInt(d[dd]) > most) most = parseInt(d[dd]);
+              if (!columns.includes(dd)) columns.push(dd);
+            }
           }
-        }
-        setMost(most);
-        let points_all = [];
-        for (const d of data) {
-          let points = [];
-          points.push(parseInt(d["timestamp"]) * 1000);
-          for (const c of columns) {
-            if (c === "time") continue;
-            let val = d[c];
-            if (val === undefined) val = 0;
-            points.push(parseInt(val));
+          setMost(most);
+          let points_all = [];
+          for (const d of data) {
+            let points = [];
+            points.push(parseInt(d["timestamp"]) * 1000);
+            for (const c of columns) {
+              if (c === "time") continue;
+              let val = d[c];
+              if (val === undefined) val = 0;
+              points.push(parseInt(val));
+            }
+            points_all.push(points);
           }
-          points_all.push(points);
-        }
-        points_all = points_all.sort(function (a, b) {
-          return a[0] - b[0];
-        });
-        setSeries(
-          new TimeSeries({
+          points_all = points_all.sort(function (a, b) {
+            return a[0] - b[0];
+          });
+          const series = new TimeSeries({
             name: "tickers",
             columns: columns,
             points: points_all,
-          })
-        );
-        setColumns(columns);
-        setStyle(styler(columns, "Paired"));
-        setLegend(columns.slice(1).map((d) => ({ key: d, label: d })));
-      });
-  }, []);
+          });
+          setTimerange(series.range());
+          setSeries(series);
+          setColumns(columns);
+          setStyle(styler(columns, "Paired"));
+          setLegend(columns.slice(1).map((d) => ({ key: d, label: d })));
+        });
+    },
+    1000 * 60 * 5,
+    true
+  );
+
+  useInterval(
+    () => {
+      fetch(
+        "https://wallstreetbets.s3-ap-southeast-2.amazonaws.com/prices.json"
+      )
+        .then((response) => response.json())
+        .then((rawData) => {
+          setUpdated(rawData.updatedAt);
+          const data = rawData.data;
+          let mostPrice = 0;
+          let columnsPrices = [];
+          columnsPrices.push("time");
+          for (const d of data) {
+            for (const dd in d) {
+              if (dd === "timestamp") continue;
+              if (parseInt(d[dd]) > mostPrice) mostPrice = parseInt(d[dd]);
+              if (!columnsPrices.includes(dd)) columnsPrices.push(dd);
+            }
+          }
+          setMostPrices(mostPrice);
+          let points_all = [];
+          for (const d of data) {
+            let points = [];
+            points.push(parseInt(d["timestamp"]) * 1000);
+            for (const c of columnsPrices) {
+              if (c === "time") continue;
+              let val = d[c];
+              if (val === undefined) val = 0;
+              points.push(parseInt(val));
+            }
+            points_all.push(points);
+          }
+          points_all = points_all.sort(function (a, b) {
+            return a[0] - b[0];
+          });
+          const seriesPrices = new TimeSeries({
+            name: "tickers",
+            columns: columnsPrices,
+            points: points_all,
+          });
+          setSeriesPrices(seriesPrices);
+          setColumnsPrices(columnsPrices);
+        });
+    },
+    1000 * 60 * 5,
+    true
+  );
 
   return (
     <div style={{ padding: 50, textAlign: "center" }}>
-      <h1>r/wallstreetbets</h1>
-      {series === null ? (
+      <h1>r/wallstreetbets comments</h1>
+      <h2>Ticker mention counts and stock price</h2>
+      {series === null || seriesPrices === null ? (
         <></>
       ) : (
         <Resizable>
           <ChartContainer
-            timeRange={series.timerange()}
+            timeRange={timerange}
             style={{
               background: "#201d1e",
               borderRadius: 8,
@@ -86,18 +148,43 @@ function App() {
             padding={20}
             paddingTop={5}
             paddingBottom={0}
-            enableDragZoom
             onBackgroundClick={() => setSelection(null)}
-            enablePanZoom={true}
+            enableDragZoom
+            onTimeRangeChanged={(timerange) => {
+              setTimerange(timerange);
+            }}
           >
-            <ChartRow height="500">
-              <YAxis id="axis1" type="linear" min={0} max={most} width="60" />
+            <ChartRow height="300" >
+              <YAxis id="axis1" type="linear" min={0} max={most} width="60" label="Ticker mention counts"/>
               <Charts>
                 <LineChart
                   axis="axis1"
                   interpolation="curveBasis"
                   series={series}
                   columns={columns}
+                  style={style}
+                  highlight={highlight}
+                  onHighlightChange={(highlight) => setHighlight(highlight)}
+                  selection={selection}
+                  onSelectionChange={(selection) => setSelection(selection)}
+                />
+              </Charts>
+            </ChartRow>
+            <ChartRow height="200" >
+              <YAxis
+                id="price"
+                type="linear"
+                min={0}
+                max={mostPrices}
+                width="60"
+                label="Stock Price ($)"
+              />
+              <Charts>
+                <LineChart
+                  axis="price"
+                  interpolation="curveBasis"
+                  series={seriesPrices}
+                  columns={columnsPrices}
                   style={style}
                   highlight={highlight}
                   onHighlightChange={(highlight) => setHighlight(highlight)}
@@ -122,7 +209,11 @@ function App() {
           onSelectionChange={(selection) => setSelection(selection)}
         />
       )}
-      <h3>by u/ejdoh1</h3>
+      <div>
+        Auto update interval 10 min, last updated <> </>
+        <TimeAgo date={updated * 1000} />
+      </div>
+      <Feedback email projectId="60162ca5d28c6c00043d7747" />
     </div>
   );
 }
